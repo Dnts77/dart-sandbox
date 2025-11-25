6552a890d32fc40a0ecc7c793ab018e3c39889a2
Dnts77 <daniel.souzadantas07@gmail.com>
2025-11-25 17:08:26 -0300

    Aula 7 - Switch

---
+++ aula7-switch/aula7.dart
@@ -0,0 +1,40 @@
+// Switch - Funciona como um IF ELSE mas com estrutura diferente, baseada em casos
+
+
+
+
+/* 
+
+switch(){
+
+}
+
+*/
+
+
+
+void main(){
+
+  var comando = "sacar";
+
+  /*
+  
+  if(comando == "depositar"){        //Exemplo simples usando if
+    print("Deposite um valor");
+  }
+  
+  */
+
+  switch(comando){
+    case "depositar":
+      print("Deposite um valor");
+      break;
+
+    case "sacar":
+      print("Faça um saque");
+      break;
+
+    default:
+      print("Nenhuma opção foi escolhida"); //Caso padrão, caso nenhuma das condições seja satisfeita. Não usa break
+  }
+}
