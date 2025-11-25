088cd0308e7a8e4fad1c514764b3f460ee1d3cda
Dnts77 <daniel.souzadantas07@gmail.com>
2025-11-25 16:09:56 -0300

    Aula 4 - Operadores Aritméticos e Operador Resumido

---
+++ aula4-operadores-aritmeticos/aula4.dart
@@ -0,0 +1,25 @@
+//Operadores aritméticos em Dart
+
+void main(){
+
+print(1+4); //Soma
+print(1-4); //Subtração
+print(4*2); //Multiplicação
+print(4/2); //Divisão
+
+//Operador resumido
+var numero = 10.0; //Se a variável não for double, não é possível usar /=
+
+//numero = numero + 10; //Incrementando valor 
+
+numero += 10;
+
+//numero -= 10;
+
+//numero *= 10;
+
+//numero /= 10;
+
+print(numero);
+
+}
