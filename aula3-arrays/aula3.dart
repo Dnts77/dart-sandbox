86f4c948575add70896eabd9eb048d759506bc65
Dnts77 <daniel.souzadantas07@gmail.com>
2025-11-25 15:55:34 -0300

    Aula 3 - Arrays

---
+++ aula3-arrays/aula3.dart
@@ -0,0 +1,17 @@
+// Arrays em Dart
+
+ 
+ void main(){
+ 
+  var nomes = [
+    150.50, 
+    "jorge", 
+     100, 
+    true
+  ]; //Criação de um Array com vários tipos de dados
+  
+  nomes[1] = "Walter"; //Acessando índice específico e mudando seu valor
+  
+  print(nomes[1]); //Pra chamar o array, usa-se o nome da variável e o índice da variável desejada
+ 
+ }
