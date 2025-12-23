f1e443fc8e651827e808166b300ea74d10ec9f3f
Dnts77 <daniel.souzadantas07@gmail.com>
2025-12-23 14:26:44 -0300

    Aula 14 - Herança

---
+++ aula14-herança/aula14.dart
@@ -0,0 +1,56 @@
+class Animal{ //Superclasse
+  String cor;
+
+  void Dormir(){
+    print("Está dormindo!");
+  }
+}
+
+
+
+
+
+
+class Cao extends Animal{ // Subclasse que herda atributos/métodos da superclasse
+
+  String? corOrelha; // As subclasses podem possuir atributos específicos
+
+  void latir(){
+    print("Latindo");
+  }
+
+}
+
+class Passaro extends Animal{ 
+
+  String? corBico;
+  
+  void voar(){
+    print("Voando");
+  }
+
+}
+
+
+
+
+void main(){
+
+  Cao cao = new Cao();
+  Passaro passaro = new Passaro();
+
+  cao.cor = "Branco";
+  print( "Cor do cão:" + cao.cor );
+  cao.latir();
+  
+  passaro.cor = "Verde";
+  print( passaro.cor );
+  passaro.voar();
+
+
+
+
+
+  
+
+}
