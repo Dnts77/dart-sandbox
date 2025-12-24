3b569a81d9d91d11a6e030aa1288761ece1f4b0a
Dnts77 <daniel.souzadantas07@gmail.com>
2025-12-24 18:47:08 -0300

    Aula 15 - sobrescrita de Métodos

---
+++ aula15-sobrescrita-de-métodos/aula15.dart
@@ -0,0 +1,54 @@
+class Animal{ 
+  String? cor;
+
+  void Dormir(){
+    print("Está dormindo!");
+  }
+
+  void Correr(){
+    print("Está correndo como um");
+  }
+}
+
+
+class Cao extends Animal{ 
+
+  String? corOrelha; 
+
+  void latir(){
+    print("Latindo");
+  }
+
+  @override // Sobrescrita do método
+  void Correr(){
+    super.Correr(); // Acessando o método da classe pai
+    print(" cão!");
+  }
+
+}
+
+class Passaro extends Animal{ 
+
+  String? corBico;
+  
+  void voar(){
+    print("Voando");
+  }
+
+  @override
+  void Correr(){
+    super.Correr();
+    print(" passáro!");
+  }
+
+}
+
+void main(){
+
+  Cao cao = new Cao();
+  Passaro passaro = new Passaro();
+
+  cao.Correr();
+  passaro.Correr();
+
+}
